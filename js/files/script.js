296b484f2e86f763da048f25297b951876f05db6
BHunterS <nvoffka2004@gmail.com>
2024-04-20 00:53:22 +0300

    feat: add html and styles 'header' block

--- js/files/script.js
+++ js/files/script.js
@@ -1,11 +1,62 @@
-document.addEventListener("DOMContentLoaded", function () {
+let bodyLockStatus = true;
+let bodyLockToggle = (delay = 500) => {
+    if (document.documentElement.classList.contains('lock')) {
+        bodyUnlock(delay);
+    } else {
+        bodyLock(delay);
+    }
+};
+let bodyUnlock = (delay = 500) => {
+    let body = document.querySelector('body');
+    if (bodyLockStatus) {
+        let lock_padding = document.querySelectorAll('[data-lp]');
+        setTimeout(() => {
+            for (let index = 0; index < lock_padding.length; index++) {
+                const el = lock_padding[index];
+                el.style.paddingRight = '0px';
+            }
+            body.style.paddingRight = '0px';
+            document.documentElement.classList.remove('lock');
+        }, delay);
+        bodyLockStatus = false;
+        setTimeout(function () {
+            bodyLockStatus = true;
+        }, delay);
+    }
+};
+
+let bodyLock = (delay = 500) => {
+    let body = document.querySelector('body');
+    if (bodyLockStatus) {
+        let lock_padding = document.querySelectorAll('[data-lp]');
+        for (let index = 0; index < lock_padding.length; index++) {
+            const el = lock_padding[index];
+            el.style.paddingRight =
+                window.innerWidth -
+                document.querySelector('.wrapper').offsetWidth +
+                'px';
+        }
+        body.style.paddingRight =
+            window.innerWidth -
+            document.querySelector('.wrapper').offsetWidth +
+            'px';
+        document.documentElement.classList.add('lock');
+
+        bodyLockStatus = false;
+        setTimeout(function () {
+            bodyLockStatus = true;
+        }, delay);
+    }
+};
+
+document.addEventListener('DOMContentLoaded', function () {
     const deadline = new Date(2023, 8, 25);
     let timerId = null;
 
-    const $days = document.querySelector(".timer__days");
-    const $hours = document.querySelector(".timer__hours");
-    const $minutes = document.querySelector(".timer__minutes");
-    const $seconds = document.querySelector(".timer__seconds");
+    const $days = document.querySelector('.timer__days');
+    const $hours = document.querySelector('.timer__hours');
+    const $minutes = document.querySelector('.timer__minutes');
+    const $seconds = document.querySelector('.timer__seconds');
 
     function countdownTimer() {
         const diff = deadline - new Date();
@@ -17,21 +68,32 @@ document.addEventListener("DOMContentLoaded", function () {
         const minutes = diff > 0 ? Math.floor(diff / 1000 / 60) % 60 : 0;
         const seconds = diff > 0 ? Math.floor(diff / 1000) % 60 : 0;
 
-        $days.textContent = days < 10 ? "0" + days : days;
-        $hours.textContent = hours < 10 ? "0" + hours : hours;
-        $minutes.textContent = minutes < 10 ? "0" + minutes : minutes;
-        $seconds.textContent = seconds < 10 ? "0" + seconds : seconds;
+        $days.textContent = days < 10 ? '0' + days : days;
+        $hours.textContent = hours < 10 ? '0' + hours : hours;
+        $minutes.textContent = minutes < 10 ? '0' + minutes : minutes;
+        $seconds.textContent = seconds < 10 ? '0' + seconds : seconds;
     }
+
+    const burger = document.querySelector('.burger');
+    const headerNav = document.querySelector('.header__nav');
+    burger.addEventListener('click', () => {
+        burger.classList.toggle('_menu-open');
+        headerNav.classList.toggle('_menu-open');
+        bodyLockToggle();
+    });
 
     countdownTimer();
 
     timerId = setInterval(countdownTimer, 1000);
 
     (function ibg() {
-        let ibg = document.querySelectorAll(".ibg");
+        let ibg = document.querySelectorAll('.ibg');
         for (var i = 0; i < ibg.length; i++) {
             if (ibg[i].querySelector('img')) {
-                ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
+                ibg[i].style.backgroundImage =
+                    'url(' +
+                    ibg[i].querySelector('img').getAttribute('src') +
+                    ')';
             }
         }
     })();
