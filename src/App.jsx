a56fdf20d41839f2d1cabe6611bbbcb2db78c68c
BHunterS <nvoffka2004@gmail.com>
2024-05-04 20:31:05 +0300

    feat: add router with home page route and delete unneccesary App.scss file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-import "./App.scss";
-import ProductSlider from "./component/ProductSlider";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
+
+import { HomePage } from "./pages/HomePage";
+
+import "./shared/scss/style.scss";
 
 function App() {
   return (
     <>
-      <ProductSlider />
+      <Header />
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </Router>
+      <Footer />
     </>
   );
 }
