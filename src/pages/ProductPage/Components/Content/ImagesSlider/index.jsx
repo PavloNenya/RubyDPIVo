1dda4e23598a2156b42dd11abda2852b2c2040b8
Ivan <tixehack@gmail.com>
2024-05-08 13:08:24 +0300

    feat: add realization ImagesSlider component of Product Page

---
+++ src/pages/ProductPage/Components/Content/ImagesSlider/index.jsx
@@ -0,0 +1,95 @@
+import { useEffect, useState } from "react";
+import secondPhoto from "../../../../../assets/img/mainscreen/productsCards/Jordans /1.png";
+import thirdPhoto from "../../../../../assets/img/mainscreen/productsCards/Jordans /2.png";
+import fourPhoto from "../../../../../assets/img/mainscreen/productsCards/Jordans /3.png";
+import fivePhoto from "../../../../../assets/img/mainscreen/productsCards/Jordans /4.png";
+import scrollingArrows from "../../../../../assets/togles/scrollingArrows.svg";
+
+import "./index.scss";
+
+const images = [
+  secondPhoto,
+  thirdPhoto,
+  fivePhoto,
+  fourPhoto,
+  thirdPhoto,
+  secondPhoto,
+];
+
+export const ImagesSlider = () => {
+  const [selectedImg, setSelectedImg] = useState(images[0]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleImageClick = (index) => {
+    setSelectedImg(images[index]);
+    setCurrentIndex(index);
+  };
+
+  const handleNextClick = () => {
+    const nextIndex = (currentIndex + 1) % images.length;
+
+    setSelectedImg(images[nextIndex]);
+    setCurrentIndex(nextIndex);
+  };
+
+  const handlePrevClick = () => {
+    const prevIndex = (currentIndex - 1 + images.length) % images.length;
+
+    setSelectedImg(images[prevIndex]);
+    setCurrentIndex(prevIndex);
+  };
+
+  useEffect(() => {
+    setSelectedImg(images[0]);
+  }, []);
+
+  return (
+    <div className="product-page__top-left">
+      <div className="product-page__photos">
+        {console.log(images)}
+        {images.map((image, index) => (
+          <img
+            key={index}
+            className={`product-page__mini-photo ${
+              index === currentIndex ? "selected" : ""
+            }`}
+            src={image}
+            alt={`mini-photo-${index}`}
+            onClick={() => handleImageClick(index)}
+          />
+        ))}
+      </div>
+
+      <div className="product-page__main-photo-wrapper">
+        <div className="infolabel infolabel-right">
+          new
+          <img className="infolabel__icon" src="/img/icons/clock.svg" alt="" />
+        </div>
+
+        <div className="product-page__togles">
+          <button
+            className="product-page__toggle-button"
+            onClick={handlePrevClick}
+          >
+            <img className="product-page__togle" src={scrollingArrows} alt="" />
+          </button>
+
+          <button
+            className="product-page__toggle-button left"
+            onClick={handleNextClick}
+          >
+            <img className="product-page__togle" src={scrollingArrows} alt="" />
+          </button>
+        </div>
+
+        <img
+          className="product-page__main-photo"
+          src={selectedImg}
+          alt="main-photo"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ImagesSlider;
--- src/pages/ProductPage/index.jsx
+++ src/pages/ProductPage/index.jsx
@@ -3,7 +3,11 @@ import { useState } from "react";
 import { BackBtn } from "../../components/BackBtn";
 import TabsContent from "./Components/Content";
 import ProductSlider from "../../components/ProductSlider";
+import btn from "../../assets/img/mainscreen/productsCards/button-2.svg";
+import btn2 from "../../assets/img/mainscreen/productsCards/button.svg";
+
 import "./index.scss";
+import ImagesSlider from "./Components/Content/ImagesSlider";
 
 export const ProductPage = () => {
   const [activeTab, setActiveTab] = useState(0);
@@ -49,71 +53,7 @@ export const ProductPage = () => {
               <div className="product-page__top-wrapper">
                 <BackBtn />
 
-                <div className="product-page__top-left">
-                  <div className="product-page__photos">
-                    <img
-                      className="product-page__mini-photo with-overlay"
-                      src="./img/product-cards/Jordans black/1.png"
-                      alt="mini-photo-1"
-                    />
-                    <img
-                      className="product-page__mini-photo"
-                      src="./img/product-cards/Jordans black/2.png"
-                      alt="mini-photo-2"
-                    />
-                    <img
-                      className="product-page__mini-photo"
-                      src="./img/product-cards/Jordans black/3.png"
-                      alt="mini-photo-3"
-                    />
-                    <img
-                      className="product-page__mini-photo"
-                      src="./img/product-cards/Jordans black/4.png"
-                      alt="mini-photo-4"
-                    />
-                    <img
-                      className="product-page__mini-photo"
-                      src="./img/product-cards/Jordans black/5.png"
-                      alt="mini-photo-5"
-                    />
-                    <img
-                      className="product-page__mini-photo"
-                      src="./img/product-cards/Jordans black/6.png"
-                      alt="mini-photo-6"
-                    />
-                  </div>
-                  <div className="product-page__main-photo-wrapper">
-                    <div className="infolabel infolabel-right">
-                      new
-                      <img
-                        className="infolabel__icon"
-                        src="/img/icons/clock.svg"
-                        alt=""
-                      />
-                    </div>
-                    <div className="product-page__togles">
-                      <a href="">
-                        <img
-                          className="product-page__togle"
-                          src="./img/product-cards/scrolling-arrows.svg"
-                          alt=""
-                        />
-                      </a>
-                      <a href="">
-                        <img
-                          className="product-page__togle left"
-                          src="./img/product-cards/scrolling-arrows.svg"
-                          alt=""
-                        />
-                      </a>
-                    </div>
-                    <img
-                      className="product-page__main-photo"
-                      src="./img/product-cards/Jordans black/main-photo.png"
-                      alt="main-photo"
-                    />
-                  </div>
-                </div>
+                <ImagesSlider />
 
                 <div className="product-page__top-right">
                   <div className="product-page__information">
@@ -127,94 +67,84 @@ export const ProductPage = () => {
                       </a>
                       <p className="product-page__sex">Unisex Shoes</p>
                     </div>
-                  </div>
-                  <div className="product-page__interactive">
+
                     <div className="product-page__price">
                       <p className="product-page__price-cost">$179</p>
                       <span className="product-page__price-cost-sale title-5">
                         $229
                       </span>
                     </div>
-                    <div className="product-page__colors">
-                      <p className="product-page__color">Color:</p>
-                      <div className="product-page__colors-img">
-                        <img
-                          className="product-page__mini-photo"
-                          src="./img/product-cards/Colors/1.png"
-                          alt=""
-                        />
-                        <img
-                          className="product-page__mini-photo"
-                          src="./img/product-cards/Colors/2.svg"
-                          alt=""
-                        />
-                        <img
-                          className="product-page__mini-photo"
-                          src="./img/product-cards/Colors/3.png"
-                          alt=""
-                        />
-                        <img
-                          className="product-page__mini-photo"
-                          src="./img/product-cards/Colors/3.png"
-                          alt=""
-                        />
+                  </div>
+
+                  <div className="product-page__interactive">
+                    <div className="product-page__interactive-top">
+                      <div className="product-page__sizes">
+                        <p className="product-page__color">Sizes:</p>
+                        <div className="product-page__sizes-wrapper">
+                          <div className="product-page__size checked">41</div>
+                          <div className="product-page__size">42</div>
+                          <div className="product-page__size">43</div>
+                          <div className="product-page__size">44</div>
+                          <div className="product-page__size">45</div>
+                        </div>
+                      </div>
+
+                      <div className="product-page__buttons">
+                        <a
+                          className="button button_lg button_default button_authorization"
+                          href="#"
+                        >
+                          buy now
+                          <span className="icon-arrow" />
+                        </a>
+
+                        <a className="product-page__button" href="">
+                          <img src={btn2} alt="" />
+                        </a>
+
+                        <a className="product-page__button" href="">
+                          <img src={btn} alt="" />
+                        </a>
+                      </div>
+
+                      <div className="product-page__dropdowns">
+                        <select
+                          className="product-page__ship-wrapper"
+                          id="size"
+                        >
+                          <option
+                            className="product-page__ship"
+                            value="Shipping & Payment"
+                          >
+                            Shipping &amp; Payment
+                          </option>
+                          <option
+                            className="product-page__ship"
+                            value="Check avaibility in store"
+                          >
+                            2
+                          </option>
+                        </select>
+                        <select
+                          className="product-page__ship-wrapper"
+                          id="size"
+                        >
+                          <option
+                            className="product-page__ship"
+                            value="Shipping & Payment"
+                          >
+                            Check avaibility in store
+                          </option>
+                          <option
+                            className="product-page__ship"
+                            value="Check avaibility in store"
+                          >
+                            2
+                          </option>
+                        </select>
                       </div>
                     </div>
-                    <div className="product-page__sizes">
-                      <p className="product-page__color">Sizes:</p>
-                      <div className="product-page__sizes-wrapper">
-                        <div className="product-page__size checked">41</div>
-                        <div className="product-page__size">42</div>
-                        <div className="product-page__size">43</div>
-                        <div className="product-page__size">44</div>
-                        <div className="product-page__size">45</div>
-                      </div>
-                    </div>
-                    <div className="product-page__buttons">
-                      <a
-                        className="button button_lg button_default button_authorization"
-                        href="#"
-                      >
-                        buy now
-                        <span className="icon-arrow" />
-                      </a>
-                      <a className="product-page__button" href="">
-                        <img src="./img/product-cards/button.svg" alt="" />
-                      </a>
-                      <a className="product-page__button" href="">
-                        <img src="./img/product-cards/button-2.svg" alt="" />
-                      </a>
-                    </div>
-                    <div className="product-page__dropdowns">
-                      <select className="product-page__ship-wrapper" id="size">
-                        <option
-                          className="product-page__ship"
-                          value="Shipping & Payment"
-                        >
-                          Shipping &amp; Payment
-                        </option>
-                        <option
-                          className="product-page__ship"
-                          value="Check avaibility in store"
-                        >
-                          2
-                        </option>
-                      </select>
-                      <select className="product-page__ship-wrapper" id="size">
-                        <option
-                          className="product-page__ship"
-                          value="Shipping & Payment"
-                        >
-                          Check avaibility in store
-                        </option>
-                        <option
-                          className="product-page__ship"
-                          value="Check avaibility in store"
-                        >
-                          2
-                        </option>
-                      </select>
-                    </div>
+
                     <p className="product-page__serial-number title-5">
                       Product code: 17150810
                     </p>
