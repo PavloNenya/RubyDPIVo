6c4734009d2942ee089fa4aa3b3844697417bfbb
BHunterS <nvoffka2004@gmail.com>
2024-05-10 19:44:18 +0300

    fix: add pagination page validation

--- src/components/Catalog/Pagination/index.jsx
+++ src/components/Catalog/Pagination/index.jsx
@@ -1,9 +1,16 @@
-import { useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useParams, useNavigate } from "react-router-dom";
 
 const Pagination = () => {
   const { page } = useParams();
+  const navigate = useNavigate();
   const [totalPages, setTotalPages] = useState(10);
+
+  useEffect(() => {
+    if (page < 1 || page > totalPages) {
+      return navigate("/catalog/1");
+    }
+  }, [navigate, page, totalPages]);
 
   return (
     <div className="products__pagination pagination">
@@ -19,24 +26,43 @@ const Pagination = () => {
             1
           </Link>
         </li>
-        <li className="pagination__item">
-          <Link to="/catalog/2" className="pagination__link">
-            2
+        <li className="pagination__item" hidden={page == 1 || page == 2}>
+          ...
+        </li>
+        <li className="pagination__item" hidden={page == 1}>
+          <Link
+            to={+page > totalPages - 2 ? `/catalog/${totalPages - 2}` : `/catalog/${page}`}
+            className="pagination__link"
+          >
+            {+page > totalPages - 2 ? totalPages - 2 : page}
           </Link>
         </li>
         <li className="pagination__item">
-          <Link to="/catalog/3" className="pagination__link">
-            3
+          <Link
+            to={+page > totalPages - 3 ? `/catalog/${totalPages - 1}` : `/catalog/${+page + 1}`}
+            className="pagination__link"
+          >
+            {+page > totalPages - 3 ? totalPages - 1 : +page + 1}
           </Link>
         </li>
-        <li className="pagination__item">...</li>
         <li className="pagination__item">
+          <Link
+            to={+page > totalPages - 2 ? `/catalog/${totalPages}` : `/catalog/${+page + 2}`}
+            className="pagination__link"
+          >
+            {+page > totalPages - 2 ? totalPages : +page + 2}
+          </Link>
+        </li>
+        <li className="pagination__item" hidden={page >= totalPages - 3}>
+          ...
+        </li>
+        <li className="pagination__item" hidden={page >= totalPages - 2}>
           <Link to={`/catalog/${totalPages}`} className="pagination__link">
             {totalPages}
           </Link>
         </li>
       </ul>
-      <Link to={`/catalog/${parseInt(page) + 1}`}>
+      <Link to={page == totalPages ? "" : `/catalog/${parseInt(page) + 1}`}>
         <button className="pagination__button right">
           <i className="pagination__icon icon-arrow-down"></i>
         </button>
